a9ff89d78c989ff80214906ac5e0dd7df882b2c2
Mehdi-Ali <alimehdi.offi@gmail.com>
2023-12-01 14:52:05 -0500

    Destabilisation is now cumulative.

--- Assets/_Scripts/Enemies/EnemyPhysics.cs
+++ Assets/_Scripts/Enemies/EnemyPhysics.cs
@@ -8,14 +8,19 @@ using UnityEngine.AI;
 public class EnemyPhysics : MonoBehaviour
 {
     [SerializeField] private float _stabilizationTime = 1.5f;
+    [SerializeField] private float _damageToDestabilizedTimeFactor = 1f;
     private Rigidbody _rb;
     private NavMeshAgent _navAgent;
     private bool _stopStabilization;
+    private Coroutine _currentStabilizeCoroutine;
+    private float _timeToWait;
+
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _navAgent = GetComponent<NavMeshAgent>();
+        _timeToWait += 0.0f;
 
     }
 
@@ -23,26 +28,40 @@ public class EnemyPhysics : MonoBehaviour
     public void TookDamage(float damage)
     {
         _stopStabilization = true;
-        SwitchControlsToRigidBody(true);
-        StartCoroutine(Stabilize(damage / 20f));
+        //SwitchControlsToRigidBody(true);
+        StartCoroutine(StabilizeAfterSeconds(damage * _damageToDestabilizedTimeFactor / 20f));
     }
 
     private void SwitchControlsToRigidBody(bool isRigidBodyInControl)
     {
-        if (isRigidBodyInControl)
-            _rb.position = _navAgent.nextPosition;
-        else
-            _navAgent.nextPosition = _rb.position;
+        // if (isRigidBodyInControl)
+        //     _rb.position = _navAgent.nextPosition;
+        // else
+        //     _navAgent.nextPosition = _rb.position;
 
-        _navAgent.updatePosition = !isRigidBodyInControl;
-        _navAgent.updateRotation = !isRigidBodyInControl;
-        _rb.isKinematic = !isRigidBodyInControl;
+        // _navAgent.updatePosition = !isRigidBodyInControl;
+        // _navAgent.updateRotation = !isRigidBodyInControl;
+        // _rb.isKinematic = !isRigidBodyInControl;
     }
 
     [Button]
-    private IEnumerator Stabilize(float seconds = 0f)
+    private IEnumerator StabilizeAfterSeconds(float seconds = 0f)
     {
-        yield return new WaitForSeconds(seconds);
+        _timeToWait += seconds;
+        
+        if (_currentStabilizeCoroutine != null)
+            StopCoroutine(_currentStabilizeCoroutine);
+
+        _currentStabilizeCoroutine = StartCoroutine(WaitAndStabilize());
+
+        yield return _currentStabilizeCoroutine;
+        _currentStabilizeCoroutine = null;
+    }
+
+    private IEnumerator WaitAndStabilize()
+    {
+        yield return new WaitForSeconds(_timeToWait);
+        _timeToWait = 0;
         StartCoroutine(StabilizeOverTime(_stabilizationTime));
     }
 
@@ -66,13 +85,13 @@ public class EnemyPhysics : MonoBehaviour
 
             if (_stopStabilization)
             {
-                SwitchControlsToRigidBody(false);
+                // SwitchControlsToRigidBody(false);
                 break;
             }
             
             yield return null;
         }
 
-        SwitchControlsToRigidBody(false);
+        // SwitchControlsToRigidBody(false);
     }
 }
